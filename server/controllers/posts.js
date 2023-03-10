8282517211aa620de702a3a28a3e6b5abb214937
ArdonitSaliji <ardonit.1980@gmail.com>
2023-03-10 20:10:13 +0100

    search added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -31,7 +31,6 @@ export const createPost = async (req, res) => {
 export const getFeedPosts = async (req, res) => {
     try {
         const post = await Post.find();
-
         res.status(200).json(post);
     } catch (err) {
         res.status(404).json({ message: err.message });
@@ -58,6 +57,7 @@ export const updatePost = async (req, res) => {
         if (description) {
             update.description = description;
         }
+
         if (picturePath) {
             update.picturePath = picturePath;
         } else {
